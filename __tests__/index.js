caf1fa68d00c3e6ca646135dbdb5252b7f0db48a
risan <risanbagja@gmail.com>
2019-01-05 18:18:25 +0100

    Add test to index.js

---
+++ __tests__/index.js
@@ -0,0 +1,24 @@
+/* global expect:false, test:false */
+const execa = require("execa");
+
+test("it throws error if command argument is missing", async () => {
+  expect.assertions(2);
+
+  try {
+    await execa("./src/index.js");
+  } catch(error) {
+    expect(error.code).toBe(1);
+    expect(error.message).toMatch(/missing/i);
+  }
+});
+
+test("it throws error if command is not available", async () => {
+  expect.assertions(2);
+
+  try {
+    await execa("./src/index.js", ["foo"]);
+  } catch(error) {
+    expect(error.code).toBe(1);
+    expect(error.message).toMatch(/not available/i);
+  }
+});
